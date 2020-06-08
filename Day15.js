fb0d8e46a7f8a8fcb1b0b66994a4be98ea661948
Zumrut-Vie <ekinci.z@gmail.com>
2020-06-08 10:07:39 +0200

    Json Exercises

---
+++ Day15.js
@@ -0,0 +1,5 @@
+/*var sandwiches = `{ "sandwich": "hamburger", "calories": "260" }`;
+var fries = `{ "fries_size": "Large French Fries", "calories": "570" }`;
+My favorite sandwich is a Hamburger which has approximately 260 calories, 
+along with it I enjoy eating Large French Fries which have about 570 calories.*/
+
